b3583235ba5dddcb63cd1fcd2fd89b57117911cc
JFer-B <bohorquez.julianf@gmail.com>
2020-10-04 11:07:58 -0500

    Punto 7 - Turno del Dealer | Punto 6 - Modificaciones

--- Game.xaml.cs
+++ Game.xaml.cs
@@ -33,7 +33,6 @@ namespace BlackJack
 
             PlayerScore = 0;
             DealerScore = 0;
-            lbPlayerScore.Content += PlayerScore.ToString();
             d = new Dealer();
             p = new Player();
             p.PlayerReady();
@@ -44,29 +43,14 @@ namespace BlackJack
             Card FirstPlayerCard = d.Deal();
             Card SecondPlayerCard = d.Deal();
             p.Init(FirstPlayerCard, SecondPlayerCard);
-            lbPCard1.Background = Brushes.White;
-            if (FirstPlayerCard.Color == "red")
-            {
-                lbPCard1.Foreground = Brushes.Red;
-            }
-            lbPCard1.Content = FirstPlayerCard.Symbol + FirstPlayerCard.Suit;
-            lbPCard2.Background = Brushes.White;
-            if (SecondPlayerCard.Color == "red")
-            {
-                lbPCard2.Foreground = Brushes.Red;
-            }
-            lbPCard2.Content = SecondPlayerCard.Symbol + SecondPlayerCard.Suit;
+            lbPlayerCards.Content = lbPlayerCards.Content + FirstPlayerCard.Symbol + FirstPlayerCard.Suit + "   " + SecondPlayerCard.Symbol + SecondPlayerCard.Suit;
             PlayerScore = PlayerScore + FirstPlayerCard.Score + SecondPlayerCard.Score;
             lbPlayerScore.Content = "Total = " + PlayerScore.ToString();
 
             Card FirstDealerCard = d.Deal();
+            lbDealerCards.Content = lbDealerCards.Content + FirstDealerCard.Symbol + FirstDealerCard.Suit;
             DealerScore = DealerScore + FirstDealerCard.Score;
-            lbDCard1.Background = Brushes.White;
-            if (FirstDealerCard.Color == "red")
-            {
-                lbDCard1.Foreground = Brushes.Red;
-            }
-            lbDCard1.Content = FirstDealerCard.Symbol + FirstDealerCard.Suit;
+            lbDealerScore.Content = "Total = " + DealerScore.ToString();
 
             if (PlayerScore == 21)
             {
@@ -78,89 +62,15 @@ namespace BlackJack
 
         }
 
-        int PlayeCardCount = 3;
         private void btnHit_Click(object sender, RoutedEventArgs e)
         {
             Card card = d.Deal();
             p.AddCard(card);
 
-            switch (PlayeCardCount)
-            {
-                case 3:
-                    lbPCard3.Background = Brushes.White;
-                    if (card.Color == "red")
-                    {
-                        lbPCard3.Foreground = Brushes.Red;
-                    }
-                    lbPCard3.Content = card.Symbol + card.Suit;
-                    break;
-                case 4:
-                    lbPCard4.Background = Brushes.White;
-                    if (card.Color == "red")
-                    {
-                        lbPCard4.Foreground = Brushes.Red;
-                    }
-                    lbPCard4.Content = card.Symbol + card.Suit;
-                    break;
-                case 5:
-                    lbPCard5.Background = Brushes.White;
-                    if (card.Color == "red")
-                    {
-                        lbPCard5.Foreground = Brushes.Red;
-                    }
-                    lbPCard5.Content = card.Symbol + card.Suit;
-                    break;
-                case 6:
-                    lbPCard6.Background = Brushes.White;
-                    if (card.Color == "red")
-                    {
-                        lbPCard6.Foreground = Brushes.Red;
-                    }
-                    lbPCard6.Content = card.Symbol + card.Suit;
-                    break;
-                case 7:
-                    lbPCard7.Background = Brushes.White;
-                    if (card.Color == "red")
-                    {
-                        lbPCard7.Foreground = Brushes.Red;
-                    }
-                    lbPCard7.Content = card.Symbol + card.Suit;
-                    break;
-                case 8:
-                    lbPCard8.Background = Brushes.White;
-                    if (card.Color == "red")
-                    {
-                        lbPCard8.Foreground = Brushes.Red;
-                    }
-                    lbPCard8.Content = card.Symbol + card.Suit;
-                    break;
-                case 9:
-                    lbPCard9.Background = Brushes.White;
-                    if (card.Color == "red")
-                    {
-                        lbPCard9.Foreground = Brushes.Red;
-                    }
-                    lbPCard9.Content = card.Symbol + card.Suit;
-                    break;
-                case 10:
-                    lbPCard10.Background = Brushes.White;
-                    if (card.Color == "red")
-                    {
-                        lbPCard10.Foreground = Brushes.Red;
-                    }
-                    lbPCard10.Content = card.Symbol + card.Suit;
-                    break;
-                case 11:
-                    lbPCard11.Background = Brushes.White;
-                    if (card.Color == "red")
-                    {
-                        lbPCard11.Foreground = Brushes.Red;
-                    }
-                    lbPCard11.Content = card.Symbol + card.Suit;
-                    break;
-            }
-            PlayeCardCount++;
+            lbPlayerCards.Content = lbPlayerCards.Content + "   " + card.Symbol + card.Suit;
             PlayerScore = PlayerScore + card.Score;
+            lbPlayerScore.Content = "Total = " + PlayerScore.ToString();
+
             if (PlayerScore > 21 && card.Score == 11)
             {
                 PlayerScore = PlayerScore - 10;
@@ -177,9 +87,20 @@ namespace BlackJack
 
         private void btnStand_Click(object sender, RoutedEventArgs e)
         {
-            if(PlayerScore > 17)
+            if(PlayerScore > 16)
             {
-                if(PlayerScore > DealerScore)
+                while (DealerScore < 17)
+                {
+                    Card card = d.Deal();
+                    lbDealerCards.Content = lbDealerCards.Content + "   " + card.Symbol + card.Suit;
+                    DealerScore = DealerScore + card.Score;
+                    if (DealerScore > 21 && card.Score == 11)
+                    {
+                        DealerScore = DealerScore - 10;
+                    }
+                }
+                lbDealerScore.Content = "Total = " + DealerScore.ToString();
+                if(PlayerScore > DealerScore || DealerScore > 21)
                 {
                     lbMatchResults.Foreground = Brushes.Yellow;
                     lbMatchResults.Content = "You Win!";
@@ -193,7 +114,7 @@ namespace BlackJack
                 btnStand.Visibility = System.Windows.Visibility.Collapsed;
             } else
             {
-                MessageBox.Show("Your Score has to be higher than 17");
+                MessageBox.Show("Your Score has to be higher than 16");
             }
         }
     }
